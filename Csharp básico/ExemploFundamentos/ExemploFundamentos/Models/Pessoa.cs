4a03fc77bff147b8def46beb52251dc875967b56
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-12-04 01:31:33 -0300

    "sintaxe basica"

---
+++ Csharp básico/ExemploFundamentos/ExemploFundamentos/Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Models
+{
+    public class Pessoa
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Name} e tenho {Age} anos.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Csharp básico/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -0,0 +1,10 @@
+﻿using ExemploFundamentos.Models;
+
+var pessoa1 = new Pessoa();
+
+Console.Write("Digite seu nome: ");
+pessoa1.Name = Console.ReadLine();
+
+Console.Write("Digite sua idade: ");
+pessoa1.Age = int.Parse(Console.ReadLine());
+pessoa1.Apresentar();
