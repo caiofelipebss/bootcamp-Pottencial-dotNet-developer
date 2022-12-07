fee0252f53e8ee7a350f192fd7654f9fd4e7477e
caio_eng.ambiental@hotmail.com <caio_eng.ambiental@hotmail.com>
2022-12-07 00:07:31 -0300

    switch case exenoki

--- Csharp básico/ExemploFundamentos/ExemploFundamentos/Program.cs
+++ Csharp básico/ExemploFundamentos/ExemploFundamentos/Program.cs
@@ -1,26 +1,81 @@
 ﻿using ExemploFundamentos.Models;
 
-int quantidadeEstoque = 3;
-int quantidadeCompra = 3;
-bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
 
+Console.WriteLine("Digite uma letra: ");
+var letra = Console.ReadLine().ToUpper();
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
-Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
-
-
-if (quantidadeCompra == 0)
+switch (letra)
 {
-    Console.WriteLine("Venda inválida!");
-}
-else if (possivelVenda)
-{
-    Console.WriteLine("Venda realizada! ");
-}
-else
-{
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada! ");
+    case "A":
+    case "B":
+    case "C":
+    case "D":
+    case "E":
+        Console.WriteLine("Vogal! ");
+        break;
+
+    default: 
+        Console.WriteLine("Não é uma vogal!");
+        break;
 }
 
-Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int quantidadeEstoque = 3;
+//int quantidadeCompra = 3;
+//bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+
+
+//Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+//Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+//Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+
+//if (quantidadeCompra == 0)
+//{
+//    Console.WriteLine("Venda inválida!");
+//}
+//else if (possivelVenda)
+//{
+//    Console.WriteLine("Venda realizada! ");
+//}
+//else
+//{
+//    Console.WriteLine("Desculpe. Não temos a quantidade desejada! ");
+//}
+
+//Console.ReadKey();
